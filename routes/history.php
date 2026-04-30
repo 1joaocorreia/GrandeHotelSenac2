49bb63dc4abd18a2fd5e6ba2a58b99678a2041fd
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2026-04-29 21:02:26 -0300

    Create history.php
    Adicionado arquivo history para rota pel api

---
+++ routes/history.php
@@ -0,0 +1,21 @@
+<?php
+
+global $conn;
+
+$user_id = 1;
+
+$sql = "SELECT * FROM reservations WHERE user_id = ? ORDER BY created_at DESC";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$reservations = [];
+
+while ($row = result_fetch_assoc()) {
+    $reservations[] = $row;
+}
+
+return jsonResponse($reservations);
