75715871e7ecb63da6b7f362002cb0bf308dc69d
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2026-05-02 12:23:05 -0300

    exibir histórico de reservas apenas para usuários logados
    Ajusta a exibição do histórico de reservas para aparecer apenas quando o usuário estiver autenticado, utilizando verificação de token no frontend.

--- src/components/History.js
+++ src/components/History.js
@@ -1,23 +1,43 @@
+ import { getToken } from "../api/authAPI.js";
+
 async function loadHistory() {
+
+    const section = document.getElementById("history-section");
+    const token = getToken();
+//  const logado = localStorage.getItem("logado");
+
+     if (!token) {
+        section.style.display = "none";
+        return;  
+    }
+
+    section.style.display = "block";
+
+    try {
+        
     const response = await fetch('/grandehotelsenac2/api/history');
     const data = await response.json();
 
     const container = document.getElementById('history');
 
     if (data.length === 0) {
-        container.innerHTML = "<p>Nenhuma reserva encontrada</p>";
+        container.innerHTML = `<p style="margin: 10px">Nenhuma reserva encontrada</p>`;
         return;
     }
 
     container.innerHTML = data.map(res => `
-        <h3 style="text-align: center;">Últimas Reservas</h3>
-        <div style="border: 1px solid #F8F9FA; padding: 10px; margin: 10px;">
+        <div style="border: 1px solid #CCC; color: #fff; padding: 10px; margin: 10px; background-color: #9DA16F; border-radius: 10px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);">
             <h3>Reserva #${res.id}</h3>
-            <p><strong>Quarto:</strong>${res.room_id}</p>
-            <p><strong>Check-in:</strong>${res.checkin}</p>
-            <p><strong>Check-out:</strong>${res.checkout}</p>
+            <p><strong>Quarto:</strong> ${res.room_id}</p>
+            <p><strong>Check-in:</strong> ${res.checkin}</p>
+            <p><strong>Check-out:</strong> ${res.checkout}</p>
         </div>
-        `).join('')
+    `).join('');
+    } catch (error) {
+        console.error("Erro ao carregar histórico:", error);
+    }
+
 }
 
 loadHistory();
